8321da7b3b19b5d0fa1663a5ce65b11271d758b5
Galaphyn <rorymb10@gmail.com>
2024-05-07 21:41:16 -0700

    Minor touch ups
    Changed speed of boolets, put shooting into its own script outside of movement to help spread things out

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,7 +16,6 @@ public class Movement : MonoBehaviour
     private bool onGround;
     private Rigidbody2D rb;
     private float jumpTimer;
-    public GameObject booletPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -51,34 +50,6 @@ public class Movement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
         transform.position = pos;
-
-        //Shooting stuff
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            //GameObject boolet = Instantiate(Resources.Load<GameObject>("Prefabs/Boolet"));
-            shootBoolet(Vector2.left);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            shootBoolet(Vector2.right);
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            shootBoolet(Vector2.up);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            shootBoolet(Vector2.down);
-        }
-
-    }
-
-    private void shootBoolet(Vector2 direction)
-    {
-        GameObject boolet = Instantiate(booletPrefab, transform.position, Quaternion.identity);
-        ProjectileLogic projectileLogic = boolet.GetComponent<ProjectileLogic>();
-        projectileLogic.Direction(direction);
-
     }
 
     void FixedUpdate()
