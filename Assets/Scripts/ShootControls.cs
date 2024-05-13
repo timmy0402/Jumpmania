92096c0c822c4cac2e9a34eb7385eaf260f42f03
timmy0402 <minhnhathb04@gmail.com>
2024-05-12 23:22:52 -0700

    added mouse shooting

--- Assets/Scripts/ShootControls.cs
+++ Assets/Scripts/ShootControls.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class ShootControls : MonoBehaviour
 {
     public GameObject booletPrefab;
+    Vector3 mousePosition;
+    Vector3 objectPosition;
+    private float angle;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Shooting stuff
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        /*if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             //GameObject boolet = Instantiate(Resources.Load<GameObject>("Prefabs/Boolet"));
             shootBoolet(Vector2.left);
@@ -31,13 +34,23 @@ public class ShootControls : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             shootBoolet(Vector2.down);
+        }*/
+        if (Input.GetMouseButtonDown(0))
+        {
+            shootBoolet();
         }
     }
-    private void shootBoolet(Vector2 direction)
+    private void shootBoolet()
     {
-        GameObject boolet = Instantiate(booletPrefab, transform.position, Quaternion.identity);
+        mousePosition = Input.mousePosition;
+        mousePosition.z = 5.23f;
+        objectPosition = Camera.main.WorldToScreenPoint(transform.position);
+        mousePosition.x = mousePosition.x - objectPosition.x;
+        mousePosition.y = mousePosition.y - objectPosition.y;
+        angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        GameObject boolet = Instantiate(booletPrefab, transform.position, Quaternion.Euler(new Vector3(0, 0, angle)));
         ProjectileLogic projectileLogic = boolet.GetComponent<ProjectileLogic>();
-        projectileLogic.Direction(direction);
+        projectileLogic.Direction(new Vector2(mousePosition.x, mousePosition.y));
 
     }
 }
