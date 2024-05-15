73139bb6dadccee74bee23d4d247243fec542c3e
caitlint-01 <caitlt3@uw.edu>
2024-05-15 10:31:21 -0700

    fixed issue with player not visible on second stage

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
         gameController = FindObjectOfType<GameController>();
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        
     }
 
     private void Update()
